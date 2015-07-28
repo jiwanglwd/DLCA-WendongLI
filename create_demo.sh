3463ef2d98d6af4cd4eabfca04d6d7fe69690cab
Zhang NS <zhanglinghan120@gmail.com>
2015-07-29 01:17:00 +0800

    shell script finished

--- create_demo.sh
+++ create_demo.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+GIF_DELAY_D2=50
+GIF_DELAY_D3=20
+GIF_LOOP=1
+GIF_FILENAME=animation
+
+# Frame per state
+D3_FPS=5
+D3_ELEV=30
+D3_AZIM_0=0
+# Increment of AZIM per frame
+D3_AZIM_FDELTA=1
+
 run () {
     DEMO_DIR=demo/D$D.L$L.N$N
     mkdir -p $DEMO_DIR
     BIN=./bin/dlca
     
     if [ -x $BIN ]; then
-        echo Running...
+        echo Running DLCA...
         $BIN $D $L $N $DEMO_DIR/D$D.L$L.N$N $P
     else
         echo $BIN not found!
@@ -15,18 +27,36 @@ run () {
     
     echo Creating visualization...
     
-    for FILE in $DEMO_DIR/*.txt
-    do
-        BASENAME=$(basename $FILE .txt)
-        echo Visualizing $BASENAME.txt
-        python visualize.py $D $L $FILE $DEMO_DIR/$BASENAME.png $M
-    done
-    
-    echo Creating gif...
+    if [ $D == 2 ]; then
+        GIF_DELAY=$GIF_DELAY_D2
+        for FILE in $DEMO_DIR/*.txt
+        do
+            BASENAME=$(basename $FILE .txt)
+            echo $BASENAME.txt
+            python plot_2d.py $L $FILE $DEMO_DIR/$BASENAME.png $M
+        done
+    elif [ $d == 3 ]; then
+        GIF_DELAY=$GIF_DELAY_D3
+        AZIM=$D3_AZIM_0
+        for FILE in $DEMO_DIR/*.txt
+        do
+            BASENAME=$(basename $FILE .txt)
+            echo $BASENAME.txt
+            i=1
+            while [ $i -le $D3_FPS ]; do
+                python plot_3d.py $L $FILE $DEMO_DIR/$BASENAME.$(printf %03d $i).png $M $D3_ELEV $AZIM
+                AZIM=$(($AZIM+$D3_AZIM_FDELTA))
+            done
+        done
+    else
+        echo Invalid dimension
+        exit 1
+    fi
     
     pushd $DEMO_DIR>/dev/null
+    echo Creating GIF...
     # ImageMagick required
-    convert -delay 50 +page *.png -loop 1 animation.gif
+    convert -delay $GIF_DELAY +page *.png -loop $GIF_LOOP $GIF_FILENAME.gif
     popd>/dev/null
 }
 
--- matlab/plot_3d.m
+++ matlab/plot_3d.m
@@ -1,11 +1,11 @@
 function plot_3d(txt_file, png_file, markersize)
     r = dlmread(txt_file);
     fig = gcf;
-%     set(fig, 'visible', 'off')
+    set(fig, 'visible', 'off')
     plot3(r(:,1), r(:,2), r(:,3), '.k', 'markersize', markersize)
     axis tight
     axis square
     % axis off
-%     print(png_file, '-dpng')
-%     close
+    print(png_file, '-dpng')
+    close
 end

3c59c1dd2d8f53a36474d3f56e0f7f2a3d9f154a
Zhang NS <zhanglinghan120@gmail.com>
2015-07-29 03:25:03 +0800

    a

--- create_demo.bat
+++ create_demo.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-Copyright (C) 2015 Zhang NS
+REM Copyright (C) 2015 Zhang NS
 
 REM This program is free software; you can redistribute it and/or modify
 REM it under the terms of the GNU General Public License as published by
